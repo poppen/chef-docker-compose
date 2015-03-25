f585bbb5c828a52718d97ebe4a3d3d6ffee01113
poppen <poppen.jp@gmail.com>
2015-03-25 23:22:58 +0900

    [from now] 2015/03/25 23:22:58
    diff --git a/test/integration/default/serverspec/docker-compose_spec.rb b/test/integration/default/serverspec/docker-compose_spec.rb
    index d805b27..493799d 100644
    --- a/test/integration/default/serverspec/docker-compose_spec.rb
    +++ b/test/integration/default/serverspec/docker-compose_spec.rb
    @@ -20,6 +20,7 @@ describe file('/etc/docker-compose.d/docker-compose.yml') do
       it { is_expected.to be_grouped_into 'root' }
    
       it { is_expected.to contain('base') }
    +  it { is_expected.to contain("'8022:22'").after('base') }
     end
    
     describe file('/etc/init/docker-compose.conf') do
    @@ -27,3 +28,7 @@ describe file('/etc/init/docker-compose.conf') do
       it { is_expected.to be_owned_by 'root' }
       it { is_expected.to be_grouped_into 'root' }
     end
    +
    +describe process('docker-compose') do
    +  it { is_expected.to be_running }
    +end

--- test/integration/default/serverspec/docker-compose_spec.rb
+++ test/integration/default/serverspec/docker-compose_spec.rb
@@ -20,10 +20,15 @@ describe file('/etc/docker-compose.d/docker-compose.yml') do
   it { is_expected.to be_grouped_into 'root' }
 
   it { is_expected.to contain('base') }
+  it { is_expected.to contain("'8022:22'").after('base') }
 end
 
 describe file('/etc/init/docker-compose.conf') do
   it { is_expected.to be_file }
   it { is_expected.to be_owned_by 'root' }
   it { is_expected.to be_grouped_into 'root' }
+end
+
+describe process('docker-compose') do
+  it { is_expected.to be_running }
 end
